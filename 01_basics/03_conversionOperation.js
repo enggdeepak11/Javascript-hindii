04e7fccf4897aaaa4c51212fc3241c326d94548e
enggdeepak11 <userdeepak62@gmail.com>
2024-09-18 11:39:13 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+
+let score = Boolean
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// boolean output
+//  {
+//     function
+//     function
+//     number
+//     NaN
+//  }
+
+// ======== Notes =============
+// "33" => 33 // it conver easly
+// "33abc" => NaN // Not a number
+// true => 1 ; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 
+// 0 => false
+ // "0" => ; is true
+ //"name" => true
+ // "" => false
+ 
