bd8e49bf856ff900afdafacb9682c322f038bb6e
enggdeepak11 <userdeepak62@gmail.com>
2024-09-20 18:53:29 +0000

    Updates in conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,14 +25,50 @@ let valueInNumber = Number(score)
 // "33abc" => NaN // Not a number
 // true => 1 ; false => 0
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 
 // 0 => false
  // "0" => ; is true
  //"name" => true
  // "" => false
+ //++++++++++++++++++++++++++ 
  
+//  let someNumber = 33
+//  let stringNumber = String(someNumber)
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+//***************************   Operations ***********************************
+
+//many basic math operation will be there 
+//like multiplication, division, plus , minus and so man
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+
+//********Problems in js */
+
+// str1 = "hitesh ";
+// str2 = " javascript"
+// str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+//Dont use rubis code , code must be readeble
+
