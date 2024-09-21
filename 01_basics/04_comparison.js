4230ab9c4d85ab584e0975c218720e4f9b350c7e
enggdeepak11 <userdeepak62@gmail.com>
2024-09-21 20:11:03 +0000

    comparision in javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(1 == 1)
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log(null > 0)  //false
+console.log(null == 0)  // false
+console.log(null >= 0)  //true
+
+console.log(undefined == 0) // false
+
+console.log(undefined == 0)
+
+
+// This type of conversion avoid please 
+// this is not a suistanable to good code
